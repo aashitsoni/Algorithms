d51ccd1bc6d32dea5b73a78645694a3ecd9a4b2e
aashitsoni <aks309@gmail.com>
2021-04-28 19:26:46 -0400

    few more commit
    About recursion and Dynamic programming.

--- Link List/SwapNodesInPairForLinkList.cpp
+++ Link List/SwapNodesInPairForLinkList.cpp
@@ -33,6 +33,49 @@ The number of nodes in the list is in the range [0, 100].
 Follow up: Can you solve the problem without modifying the values in the list's nodes? (i.e., Only nodes themselves may be changed.)
 */
 
+
+/*Recursive*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    
+    ListNode* swap(ListNode* ptr)
+    {
+        ListNode* node;
+        ListNode* tmp;
+        
+        if(ptr == NULL)
+            return NULL;
+     
+        node = ptr->next;
+        
+        if(node)
+        {
+            tmp = node->next; // node - that is next to next.
+            node->next = ptr;
+            ptr->next = swap(tmp);
+           return node;
+        }
+        return ptr;
+            
+    }
+    ListNode* swapPairs(ListNode* head) {
+        head = swap(head);
+        return head;
+        
+    }
+};
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -41,7 +84,7 @@ Follow up: Can you solve the problem without modifying the values in the list's 
  * };
  */
 
-
+/*Iterative*/
 struct ListNode* swapPairs(struct ListNode* head)
 {
     struct ListNode* prev = NULL;
